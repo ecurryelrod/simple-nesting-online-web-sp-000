461cbc8bf3a0c27bf5f3ed1830d4249d061beef0
Erika Curry-Elrod <ecurryelrod@gmail.com>
2021-04-08 21:33:06 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -57,12 +57,14 @@ def dennis_ritchies_language
      }
   programmer_hash[:dennis_ritchie][:languages] = "C"
 end
+
+def adding_matz
 # add the following information to the top level of programmer_hash
 # :yukihiro_matsumoto => {
 #   :known_for => "Ruby",
 #    :languages => ["LISP", "C"]
 # }
-def adding_matz
+
 	programmer_hash = 
  		{
         :grace_hopper => {
@@ -78,7 +80,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
-  [programmer_hash] = ":yukihiro_matsumoto"
+  programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
 end
 
 def changing_alan
