ef55924755763e599ff1ce2f20d9c41b591abfbf
Erika Curry-Elrod <ecurryelrod@gmail.com>
2021-04-08 21:14:40 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -37,7 +37,7 @@ def alan_kay_is_known_for
         }
      }
      programmer_hash[:alan_kay][:known_for]
-     programmer_hash[:dennis_ritchie][:languages.join]
+     programmer_hash[:dennis_ritchie][:languages]
 end
 
 def dennis_ritchies_language
