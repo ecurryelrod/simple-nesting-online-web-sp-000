76732da88a33b6959f6eae1a40ba18c23deadb1f
Erika Curry-Elrod <ecurryelrod@gmail.com>
2021-04-08 22:08:49 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -40,21 +40,20 @@ def alan_kay_is_known_for
 end
 
 def dennis_ritchies_language
-	programmer_hash = 
- 		{
-        :grace_hopper => {
-          :known_for => "COBOL",
-          :languages => ["COBOL", "FORTRAN"]
-        },
-        :alan_kay => {
-          :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
-        },
-        :dennis_ritchie => {
-          :known_for => "Unix",
-          :languages => ["C"]
-        }
-     }
+	programmer_hash = {
+	  :grace_hopper => {
+	    :known_for => "COBOL",
+	    :languages => ["COBOL", "FORTRAN"]
+	  },
+    :alan_kay => {
+      :known_for => "Object Orientation",
+      :languages => ["Smalltalk", "LISP"]
+    },
+    :dennis_ritchie => {
+      :known_for => "Unix",
+      :languages => ["C"]
+    }
+  }
   array = programmer_hash[:dennis_ritchie][:languages]
   array.join
 end
